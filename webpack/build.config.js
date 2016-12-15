1d39071faa72c5140a1ec820e7ede9446785d5a0
Dinesh Kumar Reddy Bogolu <DineshB@proarch.com>
2016-12-15 19:14:17 +0530

    Initial commit

---
+++ webpack/build.config.js
@@ -0,0 +1,45 @@
+const buildConfig = {
+    port: 4000,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        dist: './dist/',
+        mainJs: `${root}/app/main/app.module.js`,
+        templates: `${root}/app/**/*.html`,
+        scripts: [
+            `${root}/app/**/*.js`,
+            `!${root}/app/**/*.spec.js`
+        ],
+        tests: `${root}/app/**/*.spec.js`,
+        vendorScripts: [
+            'angular/angular.js',
+            'angular-cookies/angular-cookies.js',
+            'angular-resource/angular-resource.js',
+            'angular-aria/angular-aria.js',
+            'angular-animate/angular-animate.js',
+            'angular-material/angular-material.js',
+            'angular-messages/angular-messages.js',
+            'angular-ui-router/release/angular-ui-router.js',
+            'angular-material-data-table/dist/md-data-table.js',
+            'angular-material-data-table/dist/md-data-table-directory.js',
+            'angular-loading-bar/build/loading-bar.js',
+            'oidc-client/dist/oidc-client.js',
+            'ngstorage/ngStorage.js'
+        ],
+        styles: [
+            `${root}/sass/**/*.{scss,css}`
+        ],
+        vendorStyles: [
+            'angular-material/angular-material.css',
+            'angular-loading-bar/build/loading-bar.css',
+            'angular-material-data-table/dist/md-data-table.css'
+        ],
+        static: [
+            `${root}/index.html`,
+            `${root}/fonts/**/*`,
+            `${root}/img/**/*`,
+            `${root}/data/**/*`
+        ]
+    }
+};
+
+module.exports = buildConfig;
